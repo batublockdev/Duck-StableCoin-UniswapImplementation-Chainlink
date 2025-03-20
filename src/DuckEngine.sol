74f052d26f2485f4ce0382b014e9dcb660de2821
batublockdev <batublockchain@gmail.com>
2025-03-20 11:53:44 -0500

    new changes in the DuckEngine

--- src/DuckEngine.sol
+++ src/DuckEngine.sol
@@ -43,6 +43,18 @@ pragma solidity ^0.8.19;
  * @notice this contract is the core of the DuckCoin stablecoin system.
  */
 
+//////////////////////////////
+/////   imports           /////
+//////////////////////////////
+
+//DuckCoin Stablecoin Contract
+import {DuckCoin} from "./DuckCoin.sol";
+
+// Chainlink Aggregator Interface
+import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
 // Uniswap V3 Router Interface
 import {ISwapRouter} from "lib/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
@@ -52,7 +64,80 @@ import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.so
 // Chainlink Automation Interface
 import {AutomationCompatibleInterface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
-contract UniswapSwapper {
+contract DuckEngine is AutomationCompatibleInterface, ReentrancyGuard {
+    //////////////////////////////
+    /////   Error Messages   /////
+    //////////////////////////////
+
+    error DuckEngine_CantBeZero();
+    error DuckEngine_CollateralLenghtAndPricesFeedsMustBeEqual();
+    error DuckEngine_CollateralNotSupported();
+    error DuckEngine_FaildToTransferCollateral();
+    error DuckEngine_BreaksHelthFactor(uint256 healthFactor);
+    error DuckEngine_FaildToMint();
+
+    //////////////////////////////
+    /////   Variables       /////
+    //////////////////////////////
+
+    mapping(address token => address priceFeed) private collateralPrices;
+    mapping(address user => mapping(address token => uint256 amount))
+        private s_collateralAssets;
+    mapping(address user => uint256 amount) private s_duckCoinBalances;
+    address[] private s_collateralAssets;
+
+    uint256 private constant ADITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRICE_PRECISION = 1e18;
+    uint256 private constant LIQUIDATION_THRESHOLD = 50;
+    uint256 private constant LIQUIDATION_PRECITION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1;
+
+    DuckCoin private immutable i_duckCoin;
+
+    //////////////////////////////
+    /////   Modifier        /////
+    //////////////////////////////
+
+    modifier notZero(uint256 _amount) {
+        if (_amount == 0) {
+            revert DuckEngine_CantBeZero();
+        }
+        _;
+    }
+    modifier isAllowedCollateral(address _collateral) {
+        if (collateralPrices[_collateral] != address(0)) {
+            revert DuckEngine_CollateralNotSupported();
+        }
+    }
+    //////////////////////////////
+    /////   EVENTS          /////
+    //////////////////////////////
+
+    event CollateralDeposited(
+        address indexed user,
+        address indexed token,
+        uint256 indexed amount
+    );
+
+    //////////////////////////////
+    /////   Funtions        /////
+    //////////////////////////////
+
+    constructor(
+        address[] memory _collateralAssets,
+        address memory _pricesFeedsAddress,
+        address _duckCoin
+    ) {
+        if (_collateralAssets.length != _pricesFeedsAddress.length) {
+            revert DuckEngine_CollateralLenghtAndPricesFeedsMustBeEqual();
+        }
+        for (uint256 i = 0; i < _collateralAssets.length; i++) {
+            collateralPrices[_collateralAssets[i]] = _pricesFeedsAddress[i];
+            s_collateralAssets.push(_collateralAssets[i]);
+        }
+        i_duckCoin = DuckCoin(_duckCoin);
+    }
+
     address private constant UNISWAP_V3_ROUTER =
         0xE592427A0AEce92De3Edee1F18E0157C05861564; // Polygon Uniswap V3 Router
     ISwapRouter private constant swapRouter = ISwapRouter(UNISWAP_V3_ROUTER);
@@ -99,5 +184,112 @@ contract UniswapSwapper {
     function performUpkeep(bytes calldata) external override {
         executeSwap(1 ether, 1900 * 10 ** 6);
     }
+
     /*------------------------------------------------------*/
+
+    /**
+     *
+     * @param token the address of the token to be used as collateral
+     * @param amount the amount of the token to be deposited as collateral
+     */
+    function depositCollateral(
+        address token,
+        uint256 amount
+    ) external notZero(amount) isAllowedCollateral(token) nonReentrant {
+        s_collateralAssets[msg.sender][token] += amount;
+        emit CollateralDeposited(msg.sender, token, amount);
+        bool sucess = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        if (!sucess) {
+            revert DuckEngine_FaildToTransferCollateral();
+        }
+    }
+
+    /**
+     * @notice follows CEI
+     * @param amount the amount of DuckCoin to be minted
+     * @dev to mint DuckCoin, the user must have collateral assets deposited
+     */
+
+    function mintDuckCoin(
+        uint256 amount
+    ) external notZero(amount) nonReentrant {
+        s_duckCoinBalances[msg.sender] += amount;
+        _revetIfHealthFactorBelowThreshold(msg.sender);
+        bool sucess = i_duckCoin.mint(msg.sender, amount);
+        if (!sucess) {
+            revert DuckEngine_FaildToMint();
+        }
+    }
+
+    function _getUserInformation(
+        address user
+    ) internal view returns (uint256, uint256) {
+        return (s_duckCoinBalances[user], getCollateralValue(user));
+    }
+
+    /**
+     * @notice follows CEI
+     * @param adddress user to get the health factor
+     * @return the health factor of the user, if the health factor is below 1 the user is liquid
+     *
+     */
+    function getHealthFactor(address user) external view returns (uint256) {
+        (
+            uint256 duckCoinBalance,
+            uint256 collateralValueInUSD
+        ) = _getUserInformation(user);
+        uint256 collateralAjustedForThreshold = (collateralValueInUSD *
+            LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECITION;
+        return
+            (collateralAjustedForThreshold * PRICE_PRECISION) / duckCoinBalance;
+    }
+
+    /**
+     * @notice follows CEI
+     * @param user to check if the health factor is below the 1 otherwise revert
+     */
+    function _revetIfHealthFactorBelowThreshold(address user) internal {
+        uint256 healthFactor = getHealthFactor(user);
+        if (healthFactor < MIN_HEALTH_FACTOR) {
+            revert DuckEngine_BreaksHelthFactor(healthFactor);
+        }
+    }
+
+    /**
+     * @notice follows CEI
+     * @param adresss token to get the price in USD
+     * @param amount the amount of the token to get the price for
+     * @return the price of the token in USD
+     */
+    function getPriceUsd(
+        address token,
+        uint256 amount
+    ) external view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            collateralPrices[token]
+        );
+        (, int price, , , ) = priceFeed.latestRoundData();
+        return
+            ((uint256(price) * ADITIONAL_FEED_PRECISION) * amount) /
+            PRICE_PRECISION;
+    }
+
+    /**
+     * @notice follows CEI
+     * @param address user to get the total collateral value in USD
+     * @return the total collateral value in USD
+     */
+    function getCollateralValue(address user) public view returns (uint256) {
+        uint256 totalCollateralValueInUSD = 0;
+        for (uint256 i = 0; i < s_collateralAssets.length; i++) {
+            address token = s_collateralAssets[i];
+            uint256 amount = s_collateralAssets[user][token];
+            totalCollateralValueInUSD += getPriceUsd(token, amount);
+        }
+        return totalCollateralValueInUSD;
+    }
 }
